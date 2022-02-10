cbfc2504ca658116ffdcdcfe6a6cb27aa2f2f583
kdavies0 <93577995+kdavies0@users.noreply.github.com>
2022-02-10 15:19:50 +0200

    Created React App and set up Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        scissors_1:"hsl(39, 89%, 49%)",
+        scissors_2: "hsl(40, 84%, 53%)",
+        paper_1: "hsl(230, 89%, 62%)",
+        paper_2: "hsl(230, 89%, 65%)",
+        rock_1: "hsl(349, 71%, 52%)",
+        rock_2: "hsl(349, 70%, 56%)",
+        dark: "hsl(229, 25%, 31%)",
+        score: "hsl(229, 64%, 46%)",
+        header: "hsl(217, 16%, 45%)",
+        background_1:"hsl(214, 47%, 23%)",
+        background_2: "hsl(237, 49%, 15%)",
+      }
+    },
+  },
+  plugins: [],
+}
